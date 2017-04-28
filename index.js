4e517b6793dd0672b48faec6dd66657fc3661d72
Adrien Joly <adrien@whyd.com>
2017-04-28 10:36:59 +0200

    extra /

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var url = window.URLMap[location.pathname] || ('https://adrienjoly.com/' + location.pathname);
+var url = window.URLMap[location.pathname] || ('https://adrienjoly.com' + location.pathname);
 document.body.innerHTML = 'Redirecting to ' + url + '...';
 location.href = url + (location.search || '');
